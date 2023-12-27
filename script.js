37971bfb6063d8cbf90d02785b1e8c73be37fe79
Nick Sanchez <nicksanchez2371@gmail.com>
2023-12-27 09:24:25 -0600

    trying to add a div

---
+++ script.js
@@ -0,0 +1,33 @@
+//creating grid of 16x16 square divs
+
+
+
+
+// var existingDiv = document.getElementById('existingDiv');
+
+  // Step 2: Create a new div element
+ // var newDiv = document.createElement('div');
+
+  // Step 3 (optional): Set properties and content for the new div
+ // newDiv.setAttribute('id', 'newDiv');
+ // newDiv.innerHTML = '<p>This is the new div.</p>';
+
+  // Step 4: Append the new div to the existing div
+ // existingDiv.appendChild(newDiv);
+  
+
+
+
+
+
+
+
+
+const container = document.querySelector('container');
+const div = document.createElement("div");
+div.style.color = "red";
+div.style.width = "100px";
+div.style.height = "100px";
+div.innerHTML = '<p>this is a box</p>';
+container.appendChild(div);
+console.log(div);
