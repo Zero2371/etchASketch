7e4a5b437c2e936e7be18bf6f1c6b1680acd640f
Nick Sanchez <nicksanchez2371@gmail.com>
2023-12-28 04:57:46 -0600

    added divs with rows and boxes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
 
 
 
-
+/*
 const container = document.querySelector('container');
 const div = document.createElement("div");
 div.style.color = "red";
@@ -31,3 +31,28 @@ div.style.height = "100px";
 div.innerHTML = '<p>this is a box</p>';
 container.appendChild(div);
 console.log(div);
+*/
+let sizeOfGrid = 9;
+const container = document.querySelector('.container');
+
+
+const createGrid = (amtOfGrids) => {
+  for(let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement('div')
+      row.classList.add('grid-row')
+      
+      for(let j = 0; j < amtOfGrids; j++) {
+        const gridBox = document.createElement('div')
+        gridBox.classList.add('grid-box')
+        gridBox.addEventListener('mouseenter', () => {
+          gridBox.style.backgroundColor = 'black';
+          
+        })
+        row.appendChild(gridBox)
+      }
+
+      container.appendChild(row)
+  }
+}
+
+  createGrid(sizeOfGrid)
