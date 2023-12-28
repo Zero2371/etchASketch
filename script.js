42df57d487ebd3e87e676284e43621ccf14e7e6f
Nick Sanchez <nicksanchez2371@gmail.com>
2023-12-28 05:40:39 -0600

    prompts user for size of grid

--- script.js
+++ script.js
@@ -34,25 +34,46 @@ console.log(div);
 */
 let sizeOfGrid = 9;
 const container = document.querySelector('.container');
+const resetBtn = document.querySelector('button');
+
 
 
 const createGrid = (amtOfGrids) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
+
+
   for(let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div')
       row.classList.add('grid-row')
       
       for(let j = 0; j < amtOfGrids; j++) {
+        const widthAndHeight = 960 / amtOfGrids
         const gridBox = document.createElement('div')
         gridBox.classList.add('grid-box')
+        gridBox.style.width = `${widthAndHeight}px`
+        gridBox.style.height = `${widthAndHeight}px`
         gridBox.addEventListener('mouseenter', () => {
           gridBox.style.backgroundColor = 'black';
-          
+
         })
         row.appendChild(gridBox)
       }
-
-      container.appendChild(row)
+      wrapper.appendChild(row)
   }
+  container.appendChild(wrapper)
 }
+resetBtn.addEventListener('click', () => {
+  let userSize = Number(prompt('what grid size would you prefer'))
 
-  createGrid(sizeOfGrid)
+  while(userSize > 64) {
+  userSize = Number(prompt('pick a number of 64 or under'))
+  }
+  const wrapper = document.querySelector('.wrapper')
+  if(!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
+  }
+})
